591e1bb3ff86055b8cfc41cca6780a2c80303a83
jamae <jamae@73ec078c-4a00-0410-b6f0-efaadab75a0e>
2006-12-09 16:20:13 +0000

    Language is changed in database when user change it with flag icon
    git-svn-id: https://svn.code.sf.net/p/dvbgrab/code/trunk@171 73ec078c-4a00-0410-b6f0-efaadab75a0e

--- language.inc.php
+++ language.inc.php
@@ -1,18 +1,36 @@
 <?
 require_once("language.accept.inc.php");
 require_once("config.php");
+require_once("dolib.inc.php");
+require_once("authentication.php");
+
 // nastaveni jazykove verze (precteni udaje nastaveni prohlizece)
 
 if (!empty($_GET['lang'])) {
   $userLang = $_GET['lang'];
   setcookie("lang", $userLang, time()+60*60*24*365*2);
+  if (authenticated($_COOKIE["usr_id"], $_COOKIE["usr_pass"])) {
+    $usr_id=$_COOKIE["usr_id"];
+    $SQL = "update userinfo set usr_lang='$userLang' where usr_id=$usr_id";
+    do_sql($SQL);
+  }
 } else if (!empty($_COOKIE['lang'])) {
   $userLang = $_COOKIE['lang'];
 } else {
-  $langs=array('cs_CZ.UTF-8','en_US.UTF-8');
-  $locale=al2gt($langs, 'text/html');
-  #setlocale('LC_ALL', $locale);
-  $userLang=preg_replace("/\_.*/", "", $locale);
+  if (authenticated($_COOKIE["usr_id"], $_COOKIE["usr_pass"])) {
+    $usr_id=$_COOKIE["usr_id"];
+    $SQL = "select usr_lang from userinfo where usr_id=$usr_id";
+    $rs = do_sql($SQL);
+    if ($row = $rs->FetchRow()) {
+      $userLang=$row[0];
+    }
+  }
+  if (empty($userLang)) {
+    $langs=array('cs_CZ.UTF-8','en_US.UTF-8');
+    $locale=al2gt($langs, 'text/html');
+    #setlocale('LC_ALL', $locale);
+    $userLang=preg_replace("/\_.*/", "", $locale);
+  }
 }
 if (!empty($userLang)) {
   require_once("lang/lang.$userLang.inc.php");
--- loggers.inc.php
+++ loggers.inc.php
@@ -10,4 +10,4 @@ $logMailConf = array('subject' => 'DVBgrab log message', 'from' => _Config_from_
 $logdbg = &Log::singleton('file', _Config_dvbgrab_log, 'debug', $logFileConf);
 $logerr = &Log::singleton('file', _Config_dvbgrab_log, 'error', $logFileConf);
 $logsys  = &Log::singleton('file', _Config_dvbgrab_log.'.sys', 'sys', $logFileConf);
-$logsql  = &Log::singleton('file', _Config_dvbgrab_log.'.err', 'sql', $logFileConf);
+$logsql  = &Log::singleton('file', _Config_dvbgrab_log.'.sql', 'sql', $logFileConf);
