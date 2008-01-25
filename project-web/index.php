8f2bef9afa756cd386304141108dd83f92b567d3
jamae <jamae@73ec078c-4a00-0410-b6f0-efaadab75a0e>
2008-01-25 12:26:36 +0000

    Links to packages
    git-svn-id: https://svn.code.sf.net/p/dvbgrab/code/trunk@343 73ec078c-4a00-0410-b6f0-efaadab75a0e

--- project-web/index.php
+++ project-web/index.php
@@ -5,17 +5,26 @@ echo "<h2>Welcome on DVBgrab pages.</h2>\n";
 echo "<p>DVBgrab is a web-based application allowing registered users to request any tv show to record. When it's done download link is sent to user's e-mail. Tv schedule is from XMLTV, tv stream is saved with dumprtp and encoding is provided with mencoder.</p>\n";
 
 ?>
-Stable version is dvbgrab-1.0<br />
+Download:
+
+<a href="http://downloads.sourceforge.net/dvbgrab/dvbgrab-1.0.tar.gz?modtime=1134057187&big_mirror=0">dvbgrab-1.0.tar.gz (old version)</a>
+<br />
+<a href="http://downloads.sourceforge.net/dvbgrab/dvbgrab-2.0.tar.gz?modtime=1134057187&big_mirror=0">dvbgrab-1.0.tar.gz (recommended version)</a>
+<br />
+
+Or export from sourceforge.net subversion:
+
+dvbgrab-1.0<br />
 <code class="left">
 svn export --username anonymous https://dvbgrab.svn.sourceforge.net/svnroot/dvbgrab/tags/dvbgrab-1.0 dvbgrab
 </code>
 <br />
-Recommended version dvbgrab-2.0<br />
+dvbgrab-2.0<br />
 <code class="left">
 svn export --username anonymous https://dvbgrab.svn.sourceforge.net/svnroot/dvbgrab/tags/dvbgrab-2.0 dvbgrab
 </code>
 <br />
-Development trunk<br />
+development trunk<br />
 <code class="left">
 svn export --username anonymous https://dvbgrab.svn.sourceforge.net/svnroot/dvbgrab/trunk dvbgrab
 </code>
